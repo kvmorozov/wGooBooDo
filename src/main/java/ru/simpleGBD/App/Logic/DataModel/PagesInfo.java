691e05d1d20cd72266c5df86699e81aa99e6ef43
kmorozov <kmorozov@gmail.com>
2015-12-11 22:26:43 +0300

    missing pages output

--- src/main/java/ru/simpleGBD/App/Logic/DataModel/PagesInfo.java
+++ src/main/java/ru/simpleGBD/App/Logic/DataModel/PagesInfo.java
@@ -1,6 +1,8 @@
 package ru.simpleGBD.App.Logic.DataModel;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import ru.simpleGBD.App.Logic.ExecutionContext;
 
 import java.io.*;
@@ -15,7 +17,8 @@ public class PagesInfo implements Serializable {
 
     private static Logger logger = Logger.getLogger(PagesInfo.class.getName());
 
-    @JsonProperty("page") private PageInfo[] pages;
+    @JsonProperty("page")
+    private PageInfo[] pages;
     private Map<String, PageInfo> pagesMap;
     private LinkedList<PageInfo> pagesList;
     private String prefix;
@@ -36,20 +39,23 @@ public class PagesInfo implements Serializable {
         this.prefix = prefix;
     }
 
-    public int getPagesCount() {return pages.length;}
-
     public void build() {
         List<PageInfo> _pages = Arrays.asList(pages);
         pagesMap = new ConcurrentHashMap<>(_pages.size());
         pagesList = new LinkedList<>();
-        for(PageInfo page : _pages) {
+        for (PageInfo page : _pages) {
             pagesMap.put(page.getPid(), page);
             pagesList.add(page);
         }
     }
 
-    public PageInfo getPageByPid(String pid) {return pagesMap.get(pid);}
-    public LinkedList<PageInfo> getPages() {return pagesList;}
+    public PageInfo getPageByPid(String pid) {
+        return pagesMap.get(pid);
+    }
+
+    public LinkedList<PageInfo> getPages() {
+        return pagesList;
+    }
 
     public void exportPagesUrls() throws IOException {
         StringBuffer imgUrlsBuffer = new StringBuffer();
@@ -57,7 +63,7 @@ public class PagesInfo implements Serializable {
 
         int foundPagesCount = 0;
 
-        for(PageInfo page : pagesMap.values())
+        for (PageInfo page : pagesMap.values())
             if (page.getSig() != null) {
                 foundPagesCount++;
                 imgUrlsBuffer.append(page.getPageUrl()).append(System.getProperty("line.separator"));
@@ -68,5 +74,41 @@ public class PagesInfo implements Serializable {
         bwr.close();
 
         logger.info(String.format("Found %d sigs!", foundPagesCount));
+    }
+
+    public String getMissingPagesList() {
+        StringBuilder bList = new StringBuilder();
+        List<Pair<PageInfo, PageInfo>> pairs = new ArrayList<>();
+
+        PageInfo blockStart = null, prevPage = null, currentPage;
+
+        ListIterator<PageInfo> itr = pagesList.listIterator();
+        while (itr.hasNext()) {
+            currentPage = itr.next();
+
+            if (currentPage.dataProcessed.get())
+                if (blockStart == null) {
+                } else {
+                    pairs.add(new ImmutablePair(blockStart, prevPage));
+                    blockStart = null;
+                }
+            else if (blockStart == null)
+                blockStart = currentPage;
+            else {
+            }
+
+            if (!itr.hasNext() && blockStart != null)
+                pairs.add(new ImmutablePair(blockStart, currentPage));
+
+            prevPage = currentPage;
+        }
+
+        for (Pair<PageInfo, PageInfo> pair : pairs)
+            bList.append(String.format("%s-%s, ", pair.getLeft().getPid(), pair.getRight().getPid()));
+
+        bList.deleteCharAt(bList.length() - 1);
+        bList.deleteCharAt(bList.length() - 1);
+
+        return bList.toString();
     }
 }
