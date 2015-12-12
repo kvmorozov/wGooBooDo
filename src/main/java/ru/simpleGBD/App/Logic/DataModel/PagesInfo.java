0ce20b58ed0e10889782eeb02451a11f6a384e75
kmorozov <kmorozov@gmail.com>
2015-12-12 09:53:30 +0300

    missing pages output

--- src/main/java/ru/simpleGBD/App/Logic/DataModel/PagesInfo.java
+++ src/main/java/ru/simpleGBD/App/Logic/DataModel/PagesInfo.java
@@ -3,40 +3,24 @@ package ru.simpleGBD.App.Logic.DataModel;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
-import ru.simpleGBD.App.Logic.ExecutionContext;
 
-import java.io.*;
+import java.io.Serializable;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.logging.Logger;
 
 /**
  * Created by km on 21.11.2015.
  */
 public class PagesInfo implements Serializable {
 
-    private static Logger logger = Logger.getLogger(PagesInfo.class.getName());
+    @JsonProperty("page") private PageInfo[] pages;
+    @JsonProperty("prefix") private String prefix;
 
-    @JsonProperty("page")
-    private PageInfo[] pages;
     private Map<String, PageInfo> pagesMap;
     private LinkedList<PageInfo> pagesList;
-    private String prefix;
 
     public PageInfo[] getPagesArray() {
         return pages;
-    }
-
-    public void setPages(PageInfo[] pages) {
-        this.pages = pages;
-    }
-
-    public String getPrefix() {
-        return prefix;
-    }
-
-    public void setPrefix(String prefix) {
-        this.prefix = prefix;
     }
 
     public void build() {
@@ -55,25 +39,6 @@ public class PagesInfo implements Serializable {
 
     public LinkedList<PageInfo> getPages() {
         return pagesList;
-    }
-
-    public void exportPagesUrls() throws IOException {
-        StringBuffer imgUrlsBuffer = new StringBuffer();
-        BufferedWriter bwr = new BufferedWriter(new FileWriter(new File(ExecutionContext.outputDir.getPath() + "\\" + "urls.txt")));
-
-        int foundPagesCount = 0;
-
-        for (PageInfo page : pagesMap.values())
-            if (page.getSig() != null) {
-                foundPagesCount++;
-                imgUrlsBuffer.append(page.getPageUrl()).append(System.getProperty("line.separator"));
-            }
-
-        bwr.write(imgUrlsBuffer.toString());
-        bwr.flush();
-        bwr.close();
-
-        logger.info(String.format("Found %d sigs!", foundPagesCount));
     }
 
     public String getMissingPagesList() {
@@ -104,10 +69,12 @@ public class PagesInfo implements Serializable {
         }
 
         for (Pair<PageInfo, PageInfo> pair : pairs)
-            bList.append(String.format("%s-%s, ", pair.getLeft().getPid(), pair.getRight().getPid()));
+            if (pair.getLeft() == pair.getRight())
+                bList.append(String.format("%s, ", pair.getLeft().getPid()));
+            else
+                bList.append(String.format("%s-%s, ", pair.getLeft().getPid(), pair.getRight().getPid()));
 
-        bList.deleteCharAt(bList.length() - 1);
-        bList.deleteCharAt(bList.length() - 1);
+        bList.deleteCharAt(bList.length() - 1).deleteCharAt(bList.length() - 1);
 
         return bList.toString();
     }
