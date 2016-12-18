bcd9533d2a99a6c8a6ef07fb019f43d65292717a
kmorozov <kmorozov@gmail.com>
2016-12-18 18:18:09 +0300

    prepare to pro - libs for data api

--- ru.kmorozov.gbd.desktop/src/main/java/ru/kmorozov/gbd/desktop/gui/ManageController.java
+++ ru.kmorozov.gbd.desktop/src/main/java/ru/kmorozov/gbd/desktop/gui/ManageController.java
@@ -1,37 +1,16 @@
 package ru.kmorozov.gbd.desktop.gui;
 
-import ru.kmorozov.library.data.repository.BooksRepository;
-import ru.kmorozov.library.spring.LibraryContext;
-
 /**
  * Created by sbt-morozov-kv on 15.12.2016.
  */
 public class ManageController {
 
-    private BooksRepository booksRepository;
-    private Boolean manageAllowed;
-
-    public ManageController() {
-        try {
-            booksRepository = (BooksRepository) LibraryContext.LIBRARY_CONTEXT.getBean("booksRepository");
-        } catch (Exception ignored) {
-
-        }
-    }
-
     public boolean isManageAllowed() {
-        if (manageAllowed == null) try {
-            Class<?> clazz = Class.forName("ru.kmorozov.library.data.repository.BooksRepository");
-            manageAllowed = clazz != null;
-        } catch (ClassNotFoundException e) {
-            manageAllowed = false;
-        }
-
-        return manageAllowed;
+        return false;
     }
 
     public boolean isImportAllowed() {
-        return booksRepository != null;
+        return false;
     }
 
     public void importBooks() {
