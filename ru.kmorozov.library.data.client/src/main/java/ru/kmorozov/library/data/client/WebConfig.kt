6250fe67e1680b6968abb974175755ee5efa7521
kmorozov <kmorozov@gmail.com>
2019-05-11 19:52:38 +0300

    upd

--- ru.kmorozov.library.data.client/src/main/java/ru/kmorozov/library/data/client/WebConfig.kt
+++ ru.kmorozov.library.data.client/src/main/java/ru/kmorozov/library/data/client/WebConfig.kt
@@ -3,13 +3,13 @@ package ru.kmorozov.library.data.client
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 /**
  * Created by sbt-morozov-kv on 19.06.2017.
  */
 @Configuration
-class WebConfig : WebMvcConfigurerAdapter() {
+open class WebConfig : WebMvcConfigurer {
 
     override fun configureAsyncSupport(configurer: AsyncSupportConfigurer?) {
         configurer!!.setDefaultTimeout(java.lang.Long.MAX_VALUE)
--- ru.kmorozov.library.data.client/src/main/java/ru/kmorozov/library/data/client/WebSocketConfiguration.kt
+++ ru.kmorozov.library.data.client/src/main/java/ru/kmorozov/library/data/client/WebSocketConfiguration.kt
@@ -2,16 +2,16 @@ package ru.kmorozov.library.data.client
 
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.stereotype.Component
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 
 /**
  * Created by sbt-morozov-kv on 04.05.2017.
  */
 @Component
 @EnableWebSocketMessageBroker
-class WebSocketConfiguration : AbstractWebSocketMessageBrokerConfigurer() {
+class WebSocketConfiguration : WebSocketMessageBrokerConfigurer {
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry?) {
         registry!!.addEndpoint("/messages").withSockJS()
