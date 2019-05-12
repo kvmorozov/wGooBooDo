42596f6bd38515e342e64cd9179a9291a2f4a93c
kmorozov <kmorozov@gmail.com>
2019-05-12 22:30:48 +0300

    fix deps

--- scripts/install.cmd
+++ scripts/install.cmd
@@ -2,12 +2,18 @@ set PROJECT_ROOT=J:\Work\wGooBooDo
 set REPO=%PROJECT_ROOT%\repo
 set VERSION=2.2-b2
 
-rem ru.kmorozov.api.onedrive ru.kmorozov.gbd.core ru.kmorozov.gbd.core.config ru.kmorozov.gbd.core.logger
-rem ru.kmorozov.gbd.db ru.kmorozov.gbd.db.mongo ru.kmorozov.library.data.model
+rem ru.kmorozov.api.onedrive ru.kmorozov.gbd.core ru.kmorozov.gbd.core.config ru.kmorozov.gbd.core.logger ru.kmorozov.gbd.db ru.kmorozov.gbd.db.mongo ru.kmorozov.library.data.model ru.kmorozov.library.data.server
 
-for %%i in (ru.kmorozov.api.onedrive ru.kmorozov.gbd.core ru.kmorozov.gbd.core.config ru.kmorozov.gbd.core.logger ru.kmorozov.gbd.db ru.kmorozov.gbd.db.mongo ru.kmorozov.library.data.model ru.kmorozov.library.data.server) do (
+for %%i in () do (
     cd %PROJECT_ROOT%\%%i
     mvn compile
     mvn package -Dmaven.test.skip=true
     mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -Dfile=%PROJECT_ROOT%\%%i\target\%%i-%VERSION%.jar -DlocalRepositoryPath=%REPO%
+)
+
+for %%i in (ru.kmorozov.gbd.parent ru.kmorozov.library.parent) do (
+    cd %PROJECT_ROOT%\%%i
+rem    mvn compile
+rem    mvn package -Dmaven.test.skip=true
+    mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -Dfile=%PROJECT_ROOT%\%%i\pom.xml -DlocalRepositoryPath=%REPO%
 )
