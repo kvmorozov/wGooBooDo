36cfceaebccffb7231fda516b9955f5547ed092d
kmorozov <kmorozov@gmail.com>
2019-05-12 22:09:31 +0300

    fix deps

--- scripts/install.cmd
+++ scripts/install.cmd
@@ -5,7 +5,7 @@ set VERSION=2.2-b2
 rem ru.kmorozov.api.onedrive ru.kmorozov.gbd.core ru.kmorozov.gbd.core.config ru.kmorozov.gbd.core.logger
 rem ru.kmorozov.gbd.db ru.kmorozov.gbd.db.mongo ru.kmorozov.library.data.model
 
-for %%i in (ru.kmorozov.library.data.server) do (
+for %%i in (ru.kmorozov.api.onedrive ru.kmorozov.gbd.core ru.kmorozov.gbd.core.config ru.kmorozov.gbd.core.logger ru.kmorozov.gbd.db ru.kmorozov.gbd.db.mongo ru.kmorozov.library.data.model ru.kmorozov.library.data.server) do (
     cd %PROJECT_ROOT%\%%i
     mvn compile
     mvn package -Dmaven.test.skip=true
