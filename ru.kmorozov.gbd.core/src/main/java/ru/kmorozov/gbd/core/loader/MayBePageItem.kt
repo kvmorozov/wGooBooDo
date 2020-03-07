b8cc34205e6433b91043858b1c5f5a3abed3ec62
kmorozov <kmorozov@gmail.com>
2020-03-07 11:31:09 +0300

    updates & fixes

--- ru.kmorozov.gbd.core/src/main/java/ru/kmorozov/gbd/core/loader/MayBePageItem.kt
+++ ru.kmorozov.gbd.core/src/main/java/ru/kmorozov/gbd/core/loader/MayBePageItem.kt
@@ -1,12 +1,13 @@
 package ru.kmorozov.gbd.core.loader
 
+import ru.kmorozov.gbd.core.config.IStoredItem
 import ru.kmorozov.gbd.core.logic.model.book.base.IPage
 
 import java.io.File
 
 open class MayBePageItem : ImageItem {
 
-    var page: IPage? = null
+    var page: IPage = DUMMY_PAGE
 
     constructor(file: File) : super(file)
 
@@ -15,9 +16,30 @@ open class MayBePageItem : ImageItem {
     }
 
     open fun upgrade(page: IPage) {
-        if (this.page == null) {
+        if (this.page == DUMMY_PAGE) {
             this.page = page
             page.storedItem = this
         }
+    }
+
+    companion object DUMMY_PAGE : IPage {
+        override val pid: String
+            get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        override val order: Int
+            get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        override val isSigChecked: Boolean
+            get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        override val isDataProcessed: Boolean
+            get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        override val isFileExists: Boolean
+            get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        override val isLoadingStarted: Boolean
+            get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        override var isScanned: Boolean
+            get() = true
+            set(value) {}
+        override var storedItem: IStoredItem
+            get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+            set(value) {}
     }
 }
