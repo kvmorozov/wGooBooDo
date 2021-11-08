b502bef28cc90ff88119fe678620801398a98ac0
kmorozov <kmorozov@gmail.com>
2021-11-08 09:09:27 +0300

    Invalid CLI arguments handling

--- ru.kmorozov.gbd.desktop/src/main/java/ru/kmorozov/gbd/Main.kt
+++ ru.kmorozov.gbd.desktop/src/main/java/ru/kmorozov/gbd/Main.kt
@@ -1,5 +1,6 @@
 package ru.kmorozov.gbd
 
+import org.apache.commons.cli.ParseException
 import ru.kmorozov.gbd.core.config.CommandLineOptions
 import ru.kmorozov.gbd.core.config.GBDOptions
 import ru.kmorozov.gbd.core.config.LocalSystemOptions
@@ -14,17 +15,22 @@ internal object Main {
     @JvmStatic
     fun main(args: Array<String>) {
         if (0 < args.size) {
-            GBDOptions.init(CommandLineOptions(args))
+            try {
+                GBDOptions.init(CommandLineOptions(args))
 
-            val producer = OptionsBasedProducer()
-            ExecutionContext.initContext(1 == producer.bookIds.size)
+                val producer = OptionsBasedProducer()
+                ExecutionContext.initContext(1 == producer.bookIds.size)
 
-            val trProxy = Thread {ProxyInitiator().proxyInit()}
-            trProxy.start()
+                val trProxy = Thread { ProxyInitiator().proxyInit() }
+                trProxy.start()
 
-            ExecutionContext.INSTANCE.addBookContext(producer, PdfMaker())
+                ExecutionContext.INSTANCE.addBookContext(producer, PdfMaker())
 
-            ExecutionContext.INSTANCE.execute()
+                ExecutionContext.INSTANCE.execute()
+            }
+            catch (pe: ParseException) {
+                System.out.println("Invalid CLI arguments!")
+            }
         } else {
             GBDOptions.init(LocalSystemOptions())
             MainFrame().setVisible()
